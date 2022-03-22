fbcb3653705402078ef5faf83440b307ac42762f
wangqiang01 <wangqiang01@qianxin.com>
2022-03-22 11:00:47 +0800

    some code optimization

--- company/src/department.rs
+++ company/src/department.rs
@@ -26,19 +26,15 @@ impl Department {
         println!();
     }
 
-    pub fn add_staffs(&mut self, rookies: &Vec<Staff>) {
-        for r in rookies {
-            let nr = r.spawn();
-            self.staffs.push(nr);
-        }
+    pub fn add_staff(&mut self, rookie: &Staff) {
+        self.staffs.push(rookie.spawn());
     }
 
     // 暂时不知道引用和具体类型之间怎么转换,只能先手工copy了
     pub fn spawn(&self) -> Department {
         let mut nd = Department::new(self.name.clone(), self.manager.clone());
         for s in self.staffs.iter() {
-            let ns = s.spawn();
-            nd.staffs.push(ns);
+            nd.staffs.push(s.spawn());
         }
 
         nd
--- company/src/main.rs
+++ company/src/main.rs
@@ -4,12 +4,10 @@ fn main() {
     let mut c = Company::new(String::from("Styx Tech Ltd."), String::from("Hypnos"));
 
     let mut dept1 = company::Department::new(String::from("HR"), String::from("Homer"));
-    let mut staffs1: Vec<company::Staff> = Vec::new();
     for i in 0..10 {
-        let s = company::Staff::new(format!("HR-{}", i), i, (10 * i) as u32);
-        staffs1.push(s);
+        let s = company::Staff::new(format!("HR-{}", i), i + 20, (10 * i) as u32);
+        dept1.add_staff(&s);
     }
-    dept1.add_staffs(&staffs1);
 
     c.add_department(&dept1);
 
