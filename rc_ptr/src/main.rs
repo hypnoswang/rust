f1eec05304b8880c30d2acf08da319f7f273ed5d
wangqiang01 <wangqiang01@qianxin.com>
2022-06-08 21:18:52 +0800

    add rc practise

---
+++ rc_ptr/src/main.rs
@@ -0,0 +1,57 @@
+use std::rc::Rc;
+
+struct Person {
+    name: String,
+}
+
+impl Person {
+    fn new(n: &str) -> Person {
+        Person {
+            name: String::from(n),
+        }
+    }
+
+    fn sing(&self, song: Rc<String>) {
+        println!("{} is singing: {}", self.name, song);
+
+        eprintln!("The reference count of song is {}", Rc::strong_count(&song));
+    }
+}
+
+fn family_sing(song: Rc<String>) {
+    let families = vec![
+        Person::new("Homer"),
+        Person::new("Lisa"),
+        Person::new("Bart"),
+        Person::new("Maggie"),
+    ];
+
+    eprintln!(
+        "At beginning in fn sing, the reference count of song is {}",
+        Rc::strong_count(&song)
+    );
+
+    for p in families {
+        p.sing(Rc::clone(&song));
+    }
+
+    eprintln!(
+        "At the end in fn sing, the reference count of song is {}",
+        Rc::strong_count(&song)
+    );
+}
+
+fn main() {
+    let song = Rc::new("I love you, Lily".to_string());
+    eprintln!(
+        "At beginning in main, the reference count of song is {}",
+        Rc::strong_count(&song)
+    );
+
+    family_sing(Rc::clone(&song));
+
+    eprintln!(
+        "At the end in main, the reference count of song is {}",
+        Rc::strong_count(&song)
+    );
+}
